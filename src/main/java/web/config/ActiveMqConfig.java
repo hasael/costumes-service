ba375199de230475e265072d8a8620ddf2dfbcad
Elman Hasa <elmanhasa@Elmans-MacBook-Pro.local>
2020-12-29 15:41:28 +0000

    Add brokerurl in configurations

--- src/main/java/web/config/ActiveMqConfig.java
+++ src/main/java/web/config/ActiveMqConfig.java
@@ -1,6 +1,7 @@
 package web.config;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
@@ -10,9 +11,12 @@ import javax.jms.ConnectionFactory;
 @Configuration
 public class ActiveMqConfig {
 
+    @Value("${activemq.broker.url}")
+    private String brokerUrl;
+
     @Bean
     public ConnectionFactory connectionFactory(){
-        return new ActiveMQConnectionFactory("tcp://localhost:61616");
+        return new ActiveMQConnectionFactory(brokerUrl);
     }
 
     @Bean
